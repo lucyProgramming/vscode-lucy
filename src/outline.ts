dbcc697acb1131412eee746c837d9d81e874dabf
yu <756445638@qq.com>
2019-01-02 15:22:27 +0800

    outline packageInitBlock

--- src/outline.ts
+++ src/outline.ts
@@ -30,96 +30,103 @@ module.exports = class GoDocumentSymbolProvider implements vscode.DocumentSymbol
                     return;
                 }
                 var infos = new Array();
-                for(var i = 0 ;  i <  definitions.length ; i++ ) {
-                    var v = definitions[i];
-                    var kind : vscode.SymbolKind ;
-                    switch(v.Type) {
-                        case "variable":
-                            kind = vscode.SymbolKind.Variable;
-                            break;
-                        case "function":
-                            kind = vscode.SymbolKind.Function;
-                            break;
-                        case "constant":
-                            kind = vscode.SymbolKind.Constant;
-                            break;
-                        case "class":
-                            kind = vscode.SymbolKind.Class;
-                            break;
-                        case "enum":
-                            kind = vscode.SymbolKind.Enum;
-                            break;
-                        default:  
-                            console.log(v.name , v.Type , "have not match use default");
-                            kind = vscode.SymbolKind.Property;
-                    }
-                    var info = new vscode.DocumentSymbol(
-                        v.name , 
-                        "",
-                        kind , 
-                        new vscode.Range(
-                            new vscode.Position(v.pos.startLine , v.pos.startColumnOffset) ,
-                            new vscode.Position(v.pos.endLine , v.pos.endColumnOffset)
-                            ),
-                        new vscode.Range(
-                            new vscode.Position(v.pos.startLine , v.pos.startColumnOffset) ,
-                            new vscode.Position(v.pos.endLine , v.pos.endColumnOffset)
-                            )
-                        );
-                    infos.push(info);
-                    
-                    if (v.inners) {
-                        for(var j = 0 ;  j <  v.inners.length ; j++ ) {
-                            var vv =  v.inners[j];
-                            switch(vv.Type) {
-                                case "variable":
-                                    kind = vscode.SymbolKind.Variable;
-                                    break;
-                                case "function":
-                                    kind = vscode.SymbolKind.Function;
-                                    break;
-                                case "constant":
-                                    kind = vscode.SymbolKind.Constant;
-                                    break;
-                                case "class":
-                                    kind = vscode.SymbolKind.Class;
-                                    break;
-                                case "enum":
-                                    kind = vscode.SymbolKind.Enum;
-                                    break;
-                                case "method":
-                                    kind = vscode.SymbolKind.Method;
-                                    break;
-                                case "constructor":
-                                    kind = vscode.SymbolKind.Constructor;
-                                    break;
-                                case "field":
-                                    kind = vscode.SymbolKind.Field;
-                                    break;
-                                case "enumItem":
-                                    kind = vscode.SymbolKind.EnumMember;
-                                    break;
-                                default:  
-                                    console.log(vv.name , vv.Type , "have not match use default");
-                                    kind = vscode.SymbolKind.Property;
+                try {
+                    for(var i = 0 ;  i <  definitions.length ; i++ ) {
+                        var v = definitions[i];
+                        var kind : vscode.SymbolKind ;
+                        switch(v.Type) {
+                            case "variable":
+                                kind = vscode.SymbolKind.Variable;
+                                break;
+                            case "function":
+                                kind = vscode.SymbolKind.Function;
+                                break;
+                            case "constant":
+                                kind = vscode.SymbolKind.Constant;
+                                break;
+                            case "class":
+                                kind = vscode.SymbolKind.Class;
+                                break;
+                            case "enum":
+                                kind = vscode.SymbolKind.Enum;
+                                break;
+                            case "block":
+                                kind = vscode.SymbolKind.Function;
+                                break;
+                            default:  
+                                console.log(v.name , v.Type , "have not match use default");
+                                kind = vscode.SymbolKind.Property;
+                        }
+                        var info = new vscode.DocumentSymbol(
+                            v.name , 
+                            "",
+                            kind , 
+                            new vscode.Range(
+                                new vscode.Position(v.pos.startLine , v.pos.startColumnOffset) ,
+                                new vscode.Position(v.pos.endLine , v.pos.endColumnOffset)
+                                ),
+                            new vscode.Range(
+                                new vscode.Position(v.pos.startLine , v.pos.startColumnOffset) ,
+                                new vscode.Position(v.pos.endLine , v.pos.endColumnOffset)
+                                )
+                            );
+                        infos.push(info);
+                        
+                        if (v.inners) {
+                            for(var j = 0 ;  j <  v.inners.length ; j++ ) {
+                                var vv =  v.inners[j];
+                                switch(vv.Type) {
+                                    case "variable":
+                                        kind = vscode.SymbolKind.Variable;
+                                        break;
+                                    case "function":
+                                        kind = vscode.SymbolKind.Function;
+                                        break;
+                                    case "constant":
+                                        kind = vscode.SymbolKind.Constant;
+                                        break;
+                                    case "class":
+                                        kind = vscode.SymbolKind.Class;
+                                        break;
+                                    case "enum":
+                                        kind = vscode.SymbolKind.Enum;
+                                        break;
+                                    case "method":
+                                        kind = vscode.SymbolKind.Method;
+                                        break;
+                                    case "constructor":
+                                        kind = vscode.SymbolKind.Constructor;
+                                        break;
+                                    case "field":
+                                        kind = vscode.SymbolKind.Field;
+                                        break;
+                                    case "enumItem":
+                                        kind = vscode.SymbolKind.EnumMember;
+                                        break;
+                                    default:  
+                                        console.log(vv.name , vv.Type , "have not match use default");
+                                        kind = vscode.SymbolKind.Property;
+                                }
+                                var info2 = new vscode.DocumentSymbol(
+                                    vv.name , 
+                                    "",
+                                    kind , 
+                                    new vscode.Range(
+                                        new vscode.Position(vv.pos.startLine , vv.pos.startColumnOffset) ,
+                                        new vscode.Position(vv.pos.endLine , vv.pos.endColumnOffset)
+                                        ),
+                                    new vscode.Range(
+                                        new vscode.Position(vv.pos.startLine , vv.pos.startColumnOffset) ,
+                                        new vscode.Position(vv.pos.endLine , vv.pos.endColumnOffset)
+                                        ));
+                                info.children.push(info2);
                             }
-                            var info2 = new vscode.DocumentSymbol(
-                                vv.name , 
-                                "",
-                                kind , 
-                                new vscode.Range(
-                                    new vscode.Position(vv.pos.startLine , vv.pos.startColumnOffset) ,
-                                    new vscode.Position(vv.pos.endLine , vv.pos.endColumnOffset)
-                                    ),
-                                new vscode.Range(
-                                    new vscode.Position(vv.pos.startLine , vv.pos.startColumnOffset) ,
-                                    new vscode.Position(vv.pos.endLine , vv.pos.endColumnOffset)
-                                    ));
-                            
-                            info.children.push(info2);
                         }
                     }
+                }catch (e ){
+                    console.log(e);
                 }
+                
                 resolve(infos);
             });
         });
